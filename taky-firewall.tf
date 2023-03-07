7e75f19fdcebd841628af8b8168fd3e78eb49015
Christoffer Claesson <Christoffer.Claesson@securitybits.io>
2023-03-07 13:56:57 +0100

    added firewall rule

---
+++ taky-firewall.tf
@@ -0,0 +1,55 @@
+resource "digitalocean_firewall" "atak-docker-do" {
+
+  count = length(var.servers)
+  name = "TF-TAKY-Allow-${var.servers[count.index]}"
+  droplet_ids = [digitalocean_droplet.atak-docker-do[count.index].id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "1881"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "3000"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "8089"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "8443"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "all"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "all"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
